a4602bee3b46c5ef4e4224e24568a02aa1b43bf4
Jeffrey Hanson <jeffrey.hanson@uqconnect.edu.au>
2025-12-22 08:43:35 +1300

    add roxygen2 support

--- languages/r/injections.scm
+++ languages/r/injections.scm
@@ -1,2 +1,5 @@
 ((comment) @injection.content
   (#set! injection.language "comment"))
+((comment) @injection.content
+  (#match? @injection.content "^#' ")
+  (#set! injection.language "r_roxygen2"))
